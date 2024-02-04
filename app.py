ea51d72913ca9ba68e85d3db4dcf34fdee0c8c50
Darash Mishra <darash.mishra@encureit.com>
2024-02-04 20:36:06 +0530

    Real-Time Graph

--- app.py
+++ app.py
@@ -1,18 +1,60 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+from pyrebase import pyrebase
+import time
+import threading
 import pandas as pd
 import folium
 import sklearn
 from folium.plugins import MarkerCluster, HeatMap
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import OneHotEncoder
+import time
+import threading
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 
 app = Flask(__name__)
 
+firebase_config = {
+    "apiKey": "AIzaSyAtZb6-LRZMpCpPasCbk_vycTcRQ5fl7KA",
+    "authDomain": "dpps-23928.firebaseapp.com",
+    "databaseURL": "https://dpps-23928-default-rtdb.asia-southeast1.firebasedatabase.app",
+    "projectId": "dpps-23928",
+    "storageBucket": "dpps-23928.appspot.com",
+    "messagingSenderId": "114905554074",
+    "appId": "1:114905554074:web:d527757221e8fabe115323",
+    "measurementId": "G-ES3ZDPZC4F",
+}
+
+firebase = pyrebase.initialize_app(firebase_config)
+db = firebase.database()
+
+data_path = "/Sensor/DHT/Temperature"
+
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/realtime_data')
+def realtime_data():
+    data = db.child(data_path).get().val()
+    return jsonify(data)
+
+def update_data():
+    while True:
+        # Fetch and update data every 3 seconds
+        data = {"timestamp": int(time.time()), "value": get_updated_value()}
+        db.child(data_path).set(data)
+        time.sleep(3)
+
+def get_updated_value():
+    data = db.child("/Sensor/DHT/Temperature/value").get().val()
+    return data
+
+
+@app.route('/input_fields')
+def input():
+    return render_template('input_fields.html')
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -75,10 +117,14 @@ def predict():
         # Save the map as an HTML file
         my_map.save('templates/map_with_heatmap.html')
 
-        return render_template('result.html', prediction=prediction)
+        return render_template('index.html', prediction=prediction)
 
     except Exception as e:
         return render_template('error.html', error=str(e))
-
+    
 if __name__ == '__main__':
+    # Start a thread for updating data in the background
+    data_update_thread = threading.Thread(target=update_data)
+    data_update_thread.start()
+    # Run the Flask app
     app.run(debug=True)
